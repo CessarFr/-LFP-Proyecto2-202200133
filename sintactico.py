d922bbe64151e213403bbdd0c286ea5b78205ea0
CessarFrancisco <cessarfrancissco@gmail.com>
2024-04-28 15:51:28 -0600

    sintactico "terminado"

--- sintactico.py
+++ sintactico.py
@@ -1,11 +1,12 @@
+# sintactico.py
 class AnalizadorSintactico:
     def __init__(self):
         pass
 
     def analizar_linea(self, tokens):
-        estructura_deseada = [
+        estructura_crearbd = [
             ("PALABRA_RESERVADA", "CrearBD"),
-            ("IDENTIFICADOR", None),  
+            ("CADENA", None),  
             ("ASIGNACION", "="),
             ("PALABRA_RESERVADA", "nueva"),
             ("PALABRA_RESERVADA", "CrearBD"),
@@ -13,10 +14,75 @@ class AnalizadorSintactico:
             ("PARENTESIS_CERRADO", ")"),
             ("PUNTO_Y_COMA", ";")
         ]
+        estructura_eliminardb = [
+            ("PALABRA_RESERVADA", "EliminarBD"),
+            ("CADENA", None),
+            ("ASIGNACION", "="),
+            ("PALABRA_RESERVADA", "nueva"),
+            ("PALABRA_RESERVADA", "EliminarBD"),
+            ("PARENTESIS_ABIERTO", "("),
+            ("PARENTESIS_CERRADO", ")"),
+            ("PUNTO_Y_COMA", ";")
+        ]
+               
+        estructura_crearcoleccion = [
+            ("PALABRA_RESERVADA", "CrearColeccion"),
+            ("CADENA", None), 
+            ("ASIGNACION", "="),
+            ("PALABRA_RESERVADA", "nueva"),
+            ("PALABRA_RESERVADA", "CrearColeccion"),
+            ("PARENTESIS_ABIERTO", "("),
+            ("IDENTIFICADOR", None),  
+            ("PARENTESIS_CERRADO", ")"),
+            ("PUNTO_Y_COMA", ";")
+        ]
+        
+        estructura_eliminarcoleccion = [
+            ("PALABRA_RESERVADA", "EliminarColeccion"),
+            ("CADENA", None), 
+            ("ASIGNACION", "="),
+            ("PALABRA_RESERVADA", "nueva"),
+            ("PALABRA_RESERVADA", "EliminarColeccion"),
+            ("PARENTESIS_ABIERTO", "("),
+            ("IDENTIFICADOR", None),  
+            ("PARENTESIS_CERRADO", ")"),
+            ("PUNTO_Y_COMA", ";")
+        ]
+        
+        estructura_buscartodo = [
+            ("PALABRA_RESERVADA", "BuscarTodo"),
+            ("CADENA", None), 
+            ("ASIGNACION", "="),
+            ("PALABRA_RESERVADA", "nueva"),
+            ("PALABRA_RESERVADA", "BuscarTodo"),
+            ("PARENTESIS_ABIERTO", "("),
+            ("IDENTIFICADOR", None),  
+            ("PARENTESIS_CERRADO", ")"),
+            ("PUNTO_Y_COMA", ";")
+        ]
+        
+        estructura_buscarunico = [
+            ("PALABRA_RESERVADA", "BuscarUnico"),
+            ("CADENA", None), 
+            ("ASIGNACION", "="),
+            ("PALABRA_RESERVADA", "nueva"),
+            ("PALABRA_RESERVADA", "BuscarUnico"),
+            ("PARENTESIS_ABIERTO", "("),
+            ("IDENTIFICADOR", None),  
+            ("PARENTESIS_CERRADO", ")"),
+            ("PUNTO_Y_COMA", ";")
+        ]
+        
 
-        if self.coincide_estructura(tokens, estructura_deseada):   
+        if (self.coincide_estructura(tokens, estructura_crearbd) or
+            self.coincide_estructura(tokens, estructura_eliminardb) or
+            self.coincide_estructura(tokens, estructura_crearcoleccion) or
+            self.coincide_estructura(tokens, estructura_eliminarcoleccion) or
+            self.coincide_estructura(tokens, estructura_buscartodo) or
+            self.coincide_estructura(tokens, estructura_buscarunico)
+            ):
             return True
-        else:  
+        else:
             return False
 
     def coincide_estructura(self, tokens, estructura_deseada):
